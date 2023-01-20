86e8a4b4caaca2508f75dcff432d037f8a8f589d
Chris Robinson <awscrob@amazon.com>
2023-01-20 11:06:24 -0800

    Functional hello world & get state API call

--- src/pages/questionnaire-page.tsx
+++ src/pages/questionnaire-page.tsx
@@ -2,36 +2,54 @@ import React, { useEffect, useState } from "react";
 
 import { PageLayout } from "../components/page-layout";
 import { CodeSnippet } from "../components/code-snippet";
+import { getStateByStateHash, getStateHello } from "../services/message.service";
+//import { useNavigate } from 'react-router-dom';
 
 export const Questionnaire: React.FC = () => {
   const [message, setMessage] = useState<string>("");
 
+
+  const getMessageStateHello = async () => {
+    const { data, error } = await getStateHello();
+
+    if (data) {
+      setMessage(JSON.stringify(data, null, 2));
+    }
+
+    if (error) {
+      setMessage(JSON.stringify(error, null, 2));
+    }
+  };
+
+  const getMessageStateByHash = async (stateHash:string) => {
+    const { data, error } = await getStateByStateHash(stateHash);
+
+    if (data) {
+      setMessage(JSON.stringify(data, null, 2));
+    }
+
+    if (error) {
+      setMessage(JSON.stringify(error, null, 2));
+    }
+  };
+
   useEffect(() => {
     let isMounted = true;
 
-    const getMessage = async () => {
-      //const { data, error } = await apiCall();
-      const data = "hello"
-      const error = ""
-      if (!isMounted) {
-        return;
-      }
+    if (!isMounted) {
+      return;
+    }
 
-      if (data) {
-        setMessage(JSON.stringify(data, null, 2));
-      }
-
-      if (error) {
-        setMessage(JSON.stringify(error, null, 2));
-      }
-    };
-
-    getMessage();
+    getMessageStateHello();
 
     return () => {
       isMounted = false;
     };
   }, []);
+
+  
+  //const navigate = useNavigate();
+  //const handleHelloClick = () => navigate('/questionnaire');
 
   return (
     <PageLayout>
@@ -46,7 +64,13 @@ export const Questionnaire: React.FC = () => {
           <span>
                   This page retrieves a <strong>message</strong> from the Universal PRO Connector
                   microservice API.
-          </span>  
+          </span>
+          <p></p>
+          <h3 className="content__title">API Calls</h3>
+          <div>
+            <button onClick={getMessageStateHello}>State: Hello World</button>
+            <button onClick={() => getMessageStateByHash("abc")}>State: Get State</button>
+          </div>
           <CodeSnippet title="API Message" code={message} />      
         </div>
       </div>
---
+++ src/services/message.service.ts
@@ -0,0 +1,43 @@
+import { AxiosRequestConfig } from "axios";
+import { ApiResponse } from "../models/api-response";
+import { callExternalApi } from "./external-api.service";
+
+const apiServerUrl = process.env.REACT_APP_API_SERVER_URL + "";
+const apiXToken = process.env.REACT_APP_API_X_TOKEN  + "";
+const apiToken = process.env.REACT_APP_API_TOKEN  + "";
+
+export const getStateHello = async (): Promise<ApiResponse> => {
+  const config: AxiosRequestConfig = {
+    url: `${apiServerUrl}/state/?token=${apiToken}`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-token": apiXToken
+    },
+  };
+
+  const { data, error } = (await callExternalApi({ config })) as ApiResponse;
+
+  return {
+    data,
+    error,
+  };
+};
+
+export const getStateByStateHash = async (stateHash:string): Promise<ApiResponse> => {
+  const config: AxiosRequestConfig = {
+    url: `${apiServerUrl}/state/${stateHash}?token=${apiToken}`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-token": apiXToken
+    },
+  };
+
+  const { data, error } = (await callExternalApi({ config })) as ApiResponse;
+
+  return {
+    data,
+    error,
+  };
+};
