37d9ef495bf11af90dd558e0afd2e058ad9660ab
Chris Robinson <awscrob@amazon.com>
2023-03-23 15:32:53 -0700

    Fix notification null bug

--- src/components/notifications/notification.tsx
+++ src/components/notifications/notification.tsx
@@ -7,34 +7,43 @@ interface NotificationsProps {
 }
 
 export const Notifications: React.FC<NotificationsProps> = ({ isOpen, notifications,onClose}) => {
+    if( notifications.notifications === undefined  ) {
+        return ( <></> )
+    }
+    else {
     return ( 
-        <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-        <ModalContent>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+            <ModalContent>
 
 
-            { Object.entries(notifications.notifications).map(( [key,notification]) => (
-                <Alert 
-                status="info" 
-                variant="subtle" 
-                flexDirection="column" 
-                alignItems="center" 
-                justifyContent="center" 
-                textAlign="center" 
-                width="100%"
-                height="200px" m={4}>
-                    <AlertIcon boxSize="40px" m={0} />
-                    <AlertTitle mt={4} mb={1} fontSize="lg">
-                        {(notification as any).notification_type}
-                    </AlertTitle>
-                    <AlertDescription maxWidth="sm">
-                        {(notification as any).notification}
-                    </AlertDescription>
-                    <CloseButton position="absolute" right="8px" top="8px" onClick={onClose}/>
-                </Alert>
-                ))
-            }
-            </ModalContent>
-        </Modal>
-    );
+                { Object.entries(notifications.notifications).map(( [key,notification]:[string,any]) => (
+                    
+                            <Alert 
+                            status="info" 
+                            variant="subtle" 
+                            flexDirection="column" 
+                            alignItems="center" 
+                            justifyContent="center" 
+                            textAlign="center" 
+                            width="100%"
+                            height="200px" m={4}>
+                                <AlertIcon boxSize="40px" m={0} />
+                                <AlertTitle mt={4} mb={1} fontSize="lg">
+                                    {(notification as any).notification_type}
+                                </AlertTitle>
+                                <AlertDescription maxWidth="sm">
+                                    {(notification as any).notification}
+                                </AlertDescription>
+                                <CloseButton position="absolute" right="8px" top="8px" onClick={onClose}/>
+                            </Alert> 
+                    
+
+                        )
+                    )
+                }
+                </ModalContent>
+            </Modal>
+        );
+    }
 }
