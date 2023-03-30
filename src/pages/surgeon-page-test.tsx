cd4d4baf2587aff3ccd2fb54fab783ed8519e354
Chris Robinson <awscrob@amazon.com>
2023-03-30 08:59:33 -0700

    Init work for surgeon baseline

---
+++ src/pages/surgeon-page-test.tsx
@@ -0,0 +1,15 @@
+import { Text, VStack, Link,Box } from "@chakra-ui/react";
+
+import React, { useEffect,useState } from "react";
+
+
+export const SurgeonTester: React.FC = () => {
+
+
+    return (
+            <VStack h={500} justify="center" spacing={8}>
+
+            </VStack>
+
+    );
+}
