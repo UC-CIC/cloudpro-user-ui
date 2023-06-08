b5cc306d323ee434bc08ccb4ee15e8bcfde1aa7a
Chris Robinson <awscrob@amazon.com>
2023-06-07 19:17:55 -0700

    Initial commit for audit view

--- src/components/ptdash/survey-closed.tsx
+++ src/components/ptdash/survey-closed.tsx
@@ -1,18 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { Link } from '@chakra-ui/react'
+
 import { Box, Container, HStack, Text } from '@chakra-ui/react';
 import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
 
 export interface Props {
   description: string;
   missed: boolean;
+  sid: string;
+  duedate:string;
 }
 
-export const SurveyClosed: React.FC<Props> = ({ description, missed }) => {
+export const SurveyClosed: React.FC<Props> = ( props ) => {
+  const navigate = useNavigate();
+
+  // Navigate the user to the survey
+  const showAudit = async () => {
+    const stateHash = props.sid + props.duedate;
+    navigate(`/audit/${stateHash}`);
+  };
+
   return (
     <Container>
       <HStack spacing="10" py="2">
-        {missed ? <CloseIcon /> : <CheckIcon />}
+        {props.missed ? <CloseIcon /> : <CheckIcon />}
         <Box>
-          <Text align="left">{description}</Text>
+          <Link onClick={showAudit} >{props.description}</Link>
         </Box>
       </HStack>
     </Container>
---
+++ src/pages/audit-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Audit: React.FC = () => {
+    return (
+        <div>
+            hi
+        </div>
+    );
+}
