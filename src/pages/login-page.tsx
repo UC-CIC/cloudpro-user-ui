8b37e504b0d9cbf66e2a6e4087881dae60363e30
Eugene Kang <enarikang@gmail.com>
2023-06-25 16:04:18 -0700

    Update styling of the registration and login pages
    - Clean up the registration page.
    - Clean up the login page.
    - Fine-tune spacing and font sizing of elements in the registration and
      login pages.

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { PageLayout } from "../components/page-layout";
-import { LoginFlow } from "../components/login";
+import React from 'react';
+import { PageLayout } from '../components/page-layout';
+import { LoginFlow } from '../components/login';
 
 export const LoginPage: React.FC = () => (
   <PageLayout>
-
-      <LoginFlow/>
-
+    <LoginFlow />
   </PageLayout>
 );
--- src/pages/registered.tsx
+++ src/pages/registered.tsx
@@ -1,18 +1,58 @@
-import { Text, VStack, Link } from "@chakra-ui/react";
+import { NavLink as RouterLink } from 'react-router-dom';
+import { Container, Heading, Stack, Text, Button } from '@chakra-ui/react';
 
-import {
-    NavLink as RouterLink, // <-- import the NavLink component
-  } from "react-router-dom";
-
+import { PageLayout } from '../components/page-layout';
 
 export function Registered() {
+  return (
+    <PageLayout>
+      <Container maxW="5xl">
+        <Stack
+          textAlign="center"
+          align="center"
+          spacing={{ base: 8, md: 10 }}
+          py="16"
+        >
+          {/* Heading text */}
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', md: '5xl' }}
+            lineHeight="130%"
+            maxW="3xl"
+          >
+            Welcome to the start of your{' '}
+            <Text as="span" color="orange.400">
+              recovery journey.
+            </Text>
+          </Heading>
 
-    return (
+          {/* Secondary text */}
+          <Text
+            fontWeight={500}
+            fontSize={{ base: 'lg', sm: '2xl' }}
+            color="gray.500"
+            maxW="3xl"
+          >
+            Log in to your account to get started today.
+          </Text>
 
-            <VStack h={500} justify="center" spacing={8}>
-                <Text fontSize="5xl">Welcome</Text>
-                <Text fontSize="4xl">Registration Succeed🎉</Text>
-                <Link as={RouterLink} to="/signin">Login</Link>
-            </VStack>
-    );
+          {/* Login button */}
+          <Stack spacing={6} direction={'row'}>
+            <Button
+              colorScheme="orange"
+              bg="orange.400"
+              _hover={{ bg: 'orange.500' }}
+              rounded="full"
+              px={12}
+              as={RouterLink}
+              size="lg"
+              to="/signin"
+            >
+              Login
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </PageLayout>
+  );
 }
